9fb0a6b385bff9b897f1846043f6723e8132f85f
ideadapt <kunz@ideadapt.net>
2015-01-24 14:50:59 +0100

    dist font

--- Gruntfile.js
+++ Gruntfile.js
@@ -319,7 +319,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*',
+            'fonts/{,*/}*.*',
             'data/*.csv'
           ]
         }, {
